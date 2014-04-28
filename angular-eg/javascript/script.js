6b71568039e6398549ff25e41736fc1acc6cb9f5
Apoorv Parijat <apoorvparijat@gmail.com>
2014-04-28 16:07:23 +0530

    Data being loaded from remote. Added sorting and filtering functionality.

--- angular-eg/javascript/script.js
+++ angular-eg/javascript/script.js
@@ -3,18 +3,24 @@ var app = angular.module('calculatorApp', ['ui.router']);
 
 
 app.config(['$httpProvider', '$stateProvider', '$urlRouterProvider', function($httpProvider, $stateProvider, $urlRouterProvider){
-		$urlRouterProvider.otherwise('/');
+		//$urlRouterProvider.otherwise('/');
 
-		$stateProvider.state('home', {
-        	url: '/',
-        	templateUrl: 'templates/home.html'
+		$stateProvider.state('display', {
+        	url: '/display',
+        	templateUrl: 'templates/display.html',
+			controller: 'DisplayAttendance',
+			resolve: {
+				'subject': ['$http', function($http) {
+					return $http.get('javascript/data.html')
+				}]
+			}
     	});	
 
     	$stateProvider.state('calculate', {
         	url: '/calculate',
         	templateUrl: 'templates/calculate.html',
         	controller: 'MainCtrl'
-    	});	
+    	});
 }]);
 
 app.controller('MainCtrl', ['$scope', function ($scope){
@@ -25,5 +31,12 @@ app.controller('MainCtrl', ['$scope', function ($scope){
 
 		$scope.$watch('firstNo', addFn);
 		$scope.$watch('secondNo', addFn);
+	}]
+);
+
+app.controller('DisplayAttendance', ['$scope', '$http', 'subject', function ($scope,$http, subject){
+		$scope.predicate = 'name';
+		$scope.reverse = false;
+		$scope.subject = subject.data;
 	}]
 );
